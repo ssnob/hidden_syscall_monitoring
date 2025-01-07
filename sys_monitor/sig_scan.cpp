0852ee838d016463901d3c7fe27acb81cd983fc7
ssno <86132963+ssnob@users.noreply.github.com>
2025-01-06 18:49:35 -0600

    lc

--- sys_monitor/sig_scan.cpp
+++ sys_monitor/sig_scan.cpp
@@ -1,3 +1,19 @@
+/*
+   Copyright 2025 ssnob
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #include "sig_scan.h"
 
 unsigned __int64 sig_scan::find_signature(unsigned __int64 start, unsigned __int32 size, unsigned char* bytes, unsigned __int32 byte_size)
